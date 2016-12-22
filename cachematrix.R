df87ab8de565c131e094a2004c8ae07c4f9937ab
Bhuvana Mitra Guturu <bhuvan-3031@bhuvan-3031.csez.zohocorpin.com>
2016-12-22 20:31:30 +0530

    modifying cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m = matrix() 	#create am empty matrix to store the inverse
+	m <- NULL
+    	
+	set <- function(y = matrix()){
+        	x <<- y		
+		m <<- NULL
+	}
+	
+	get <- function() x
+	
+	setInverse <- function(solve) m <<- solve	#this will keep inverse in cache
+	
+	getInverse <- function() m			#if inverse is in cache, return it
+	
+	list(set = set, get = get, 
+		setInverse = setInverse,
+		getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getInverse()				#try to get inverse from cache
+	if(!is.null(m)){
+      		message("getting cached data")
+          	return(m)				#return the value if present in cache
+	}
+	
+	data <- x$get()					#if not present, get the matrix
+	
+	m <- solve(data, ...)				#find the inverse of it
+	
+	x$setInverse(m)					#push it to cache
+	
+	m						#print to console
 }
